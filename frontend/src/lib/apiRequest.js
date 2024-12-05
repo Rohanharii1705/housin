d51ba791e5b2a303478b33d4f9e247d5dcbda516
Rohanharigovind <rohanharigovind@gmail.com>
2024-12-05 11:47:47 +0530

    Update apiRequest.js

--- frontend/src/lib/apiRequest.js
+++ frontend/src/lib/apiRequest.js
@@ -1,3 +1,4 @@
+// verifyToken.js
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
@@ -6,10 +7,10 @@ export const verifyToken = (req, res, next) => {
     if (!token) return res.status(401).json({ message: "Not Authenticated" });
 
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, payload) => {
-        if (err) return res.status(403).json({ message: "Token is not valid or expired!" });
+        if (err) return res.status(403).json({ message: "Token is not valid!" });
 
         req.userId = payload.id;
-        req.isAdmin = payload.isAdmin;
+        req.isAdmin = payload.isAdmin; 
         next();
     });
 };
